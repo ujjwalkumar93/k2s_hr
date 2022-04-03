2ef939ade1900c8f3aaa7680dc7804b38a0e6a75
ujjwalkumar93 <pathakujjwal93@gmail.com>
2022-04-03 20:25:59 +0530

    updated

--- k2s_hr/public/js/employee.js
+++ k2s_hr/public/js/employee.js
@@ -1,15 +1,34 @@
 frappe.ui.form.on("Employee", {
     refresh: function(frm){
         let today = new Date();
-        frappe.db.get_value("Employee Checkin", {"employee": frm.doc.name,"date":today}, "name", (r) => {
-            console.log(">>>>>>>>>>>>>>1111",r)
-            if(r.name){
-                create_check_out_btn(frm)
-            }
-            else{
-                create_check_in_btn(frm)
-            }
-        });
+        // frappe.db.get_list("Employee Checkin", {"employee": frm.doc.name,"date":today,order_by: "creation desc"}, "log_type", (r) => {
+        //     console.log(">>>>>>>>>>>>>>1111",r)
+        //     if(r.name){
+        //         create_check_out_btn(frm)
+        //     }
+        //     else{
+        //         create_check_in_btn(frm)
+        //     }
+        // });
+        frappe.db
+				.get_list("Employee Checkin", {
+					filters: {"employee": frm.doc.name,"date":today},
+					fields: ["name", "log_type"],
+					order_by: "creation desc",
+					limit: 1,
+				})
+				.then((result) => {
+					if (result.length > 0) {
+						if(result[0].log_type === "IN"){
+                            create_check_out_btn(frm)
+                        }
+                        if(result[0].log_type === "OUT"){
+                            create_check_in_btn(frm)
+                        }
+					} else {
+                        create_check_in_btn(frm)
+                    }
+				});
     }
 })
 
